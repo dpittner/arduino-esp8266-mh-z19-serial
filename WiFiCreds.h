dc8f81e06497d1dad8c132977cd66fbe21e4307f
daniel pittner <daniel.pittner.84@googlemail.com>
2018-01-02 16:27:41 +0100

    Initial commit
    refactor code into files
    
    refactor
    
    fix: remove password

---
+++ WiFiCreds.h
@@ -0,0 +1,2 @@
+char ssid[] = "BinaryWave";     //  your network SSID (name)
+char pass[] = "...";  // your network password
---
+++ bme280.h
@@ -0,0 +1,44 @@
+#include <BME280I2C.h>
+
+struct bmeData {
+  float temp;
+  float hum;
+  float pres;
+};
+
+BME280I2C bme; 
+
+void setupBme() {
+
+  Wire.begin(D2, D1);
+
+  while(!bme.begin())
+  {
+    Serial.println("Could not find BME280 sensor!");
+    delay(1000);
+  }
+
+  switch(bme.chipModel())
+  {
+     case BME280::ChipModel_BME280:
+       Serial.println("Found BME280 sensor! Success.");
+       break;
+     case BME280::ChipModel_BMP280:
+       Serial.println("Found BMP280 sensor! No Humidity available.");
+       break;
+     default:
+       Serial.println("Found UNKNOWN sensor! Error!");
+  }
+}
+
+bmeData readBme()
+{
+   float temp(NAN), hum(NAN), pres(NAN);
+
+   BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
+   BME280::PresUnit presUnit(BME280::PresUnit_Pa);
+
+   bme.read(pres, temp, hum, tempUnit, presUnit);
+
+   return (bmeData) {temp, hum, pres};
+}
